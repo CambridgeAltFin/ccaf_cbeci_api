20eaee8657871830af27b7796275b28bee998d34
adrear <kirillmanakhov2306@gmail.com>
2021-05-21 18:28:06 +0300

    api

--- frontend/plugins/fireinit.js
+++ frontend/plugins/fireinit.js
@@ -4,14 +4,14 @@ import 'firebase/database'
 import 'firebase/storage'
 
 const config = {
-  apiKey: 'AIzaSyD_iVNWYedOALjoiDn9YRZj9B267RJEtSM',
-  authDomain: 'ccaf-afea-test.firebaseapp.com',
-  databaseURL: 'https://ccaf-afea-test.firebaseio.com',
-  projectId: 'ccaf-afea-test',
-  storageBucket: 'ccaf-afea-test.appspot.com',
-  messagingSenderId: '936638856157',
-  appId: '1:936638856157:web:36c3c80994aae098525434',
-  measurementId: 'G-8YY657KG1C'
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID
 }
 
 if (!firebase.apps.length) { firebase.initializeApp(config) }

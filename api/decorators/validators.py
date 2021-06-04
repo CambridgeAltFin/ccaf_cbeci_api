21f28f352bbe8b958afbf7981033407aecea43d1
chesanovskiy_dv <chesanovskiydv@gmail.com>
2021-06-04 15:30:41 +0300

    update error messages

--- api/decorators/validators.py
+++ api/decorators/validators.py
@@ -1,11 +1,19 @@
 from functools import wraps
-from flask import request
+from flask import request, jsonify
+
 
 def validate(schema):
     def decorator(f):
         @wraps(f)
         def decorated_function(*args, **kwargs):
-            schema.validate(request.json)
+            try:
+                schema.validate(request.json)
+            except Exception as error:
+                if len(error.autos) > 1:
+                    message = str(error.autos[-1])
+                    if message.startswith('Missing key:'):
+                        return jsonify(error=message)
+                raise error
 
             return f(*args, **kwargs)
 

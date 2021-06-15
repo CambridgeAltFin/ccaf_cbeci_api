b24685c0848f9a134e880741fc73069c2f28ff5a
chesanovskiy_dv <chesanovskiydv@gmail.com>
2021-06-15 17:06:46 +0300

    update countries api

--- api/chart_API.py
+++ api/chart_API.py
@@ -337,15 +337,16 @@ def recalculate_guess(value):
 def countries_btc():
     tup2dict = {a:[c,d,b] for a,b,c,d,e,f in countries}
     tup2dict['Bitcoin'][0] = round(cons[-1][4],2)
-    dictsort = sorted(tup2dict.items(), key = lambda i: i[1][0], reverse=True)
+    dictsort = sorted(tup2dict.items(), key = lambda i: -1 if i[1][0] is None else i[1][0], reverse=True)
     response = []
     for item in dictsort:
+         electricity_consumption = 0 if item[1][0] is None else item[1][0]
          response.append({
             'country': item[0],
             'code': item[1][2],
-            'y': item[1][0],
+            'y': electricity_consumption,
             'x': dictsort.index(item)+1,
-            'bitcoin_percentage': round(item[1][0]/round(cons[-1][4], 2)*100, 2),
+            'bitcoin_percentage': round(electricity_consumption/round(cons[-1][4], 2)*100, 2),
             'logo': item[1][1]
             })
     for item in response:

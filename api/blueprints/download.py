9ed51d3b9d41335fd2cbbe56d5f659b80cba65f5
chesanovskiy_dv <chesanovskiydv@gmail.com>
2021-07-07 14:42:34 +0300

    add methods for download csv file with charts data

--- api/blueprints/download.py
+++ api/blueprints/download.py
@@ -4,6 +4,7 @@ import csv
 import io
 from datetime import datetime
 from services import EnergyConsumption, EnergyConsumptionByTypes
+from queries import get_mining_countries, get_mining_provinces
 
 bp = Blueprint('download', __name__, url_prefix='/download')
 
@@ -39,7 +40,7 @@ def send_file(first_line=None, file_type='csv'):
     def send_csv(headers: Dict[str, str], rows: List[Dict[str, Union[str, int, float]]], filename='export.csv'):
         si = io.StringIO()
         keys = headers.keys()
-        cw = csv.DictWriter(si, fieldnames=keys)
+        cw = csv.DictWriter(si, fieldnames=keys, extrasaction="ignore")
         if first_line is not None and len(keys) > 0:
             cw.writerow({list(keys)[0]: first_line})
         cw.writerow(headers)
@@ -53,7 +54,7 @@ def send_file(first_line=None, file_type='csv'):
 
 
 @bp.route('/data')
-def download(version=None):
+def data(version=None):
     file_type = request.args.get('file_type', 'csv')
     price = request.args.get('price', 0.05)
     headers = {
@@ -65,5 +66,38 @@ def download(version=None):
     }
     rows = get_data(version, float(price))
     send_file_func = send_file(first_line=f'Average electricity cost assumption: {price} USD/kWh', file_type=file_type)
+
+    return send_file_func(headers, rows)
+
+@bp.route('/mining_countries')
+def mining_countries(version=None):
+    if version != 'v1.1.0':
+        raise NotImplementedError('Not Implemented')
+
+    file_type = request.args.get('file_type', 'csv')
+    headers = {
+        'date': 'Date',
+        'name': 'Country',
+        'value': 'Value',
+    }
+    rows = get_mining_countries()
+    send_file_func = send_file(file_type=file_type)
+
+    return send_file_func(headers, rows)
+
+@bp.route('/mining_provinces')
+def mining_provinces(version=None):
+    if version != 'v1.1.0':
+        raise NotImplementedError('Not Implemented')
+
+    file_type = request.args.get('file_type', 'csv')
+    headers = {
+        'date': 'Date',
+        'name': 'Province',
+        'value': 'Value',
+        'local_value': 'Local Value'
+    }
+    rows = get_mining_provinces()
+    send_file_func = send_file(file_type=file_type)
 
     return send_file_func(headers, rows)

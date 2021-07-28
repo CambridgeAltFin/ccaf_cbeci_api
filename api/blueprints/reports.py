2062b3a1577195408734167ec712ba4bb920332f
chesanovskiy_dv <chesanovskiydv@gmail.com>
2021-07-28 12:31:00 +0300

    add fileurl prop

--- api/blueprints/reports.py
+++ api/blueprints/reports.py
@@ -1,19 +1,33 @@
+import datetime
+from config import BUCKETS, FOLDERS
 from flask import Blueprint, jsonify, request, abort
+from firebase_admin import storage
 from services.realtime_collection import realtime_collections, Collections
 
 bp = Blueprint('reports.py', __name__, url_prefix='/reports.py')
 
 
+def to_obj(doc):
+    doc_copy = doc.copy()
+    doc_copy['fileurl'] = [
+        storage.bucket(BUCKETS['default']) \
+            .blob(f'{FOLDERS["reports"]}/{doc["filename"]}') \
+            .generate_signed_url(
+            expiration=datetime.timedelta(days=1),
+        )
+    ]
+
+    return doc_copy
+
 @bp.route('/')
 def index():
-    pass
     project = request.args.get('project', 'cbeci')
     docs = realtime_collections.collections[Collections.REPORTS].get()
 
     def docs_filter(doc):
         return doc.get('is_active') == True and doc.get('project') == project
 
-    return jsonify(data=list(sorted(filter(docs_filter, docs), key=lambda doc: doc.get('order_position'))))
+    return jsonify(data=list(sorted(map(to_obj, filter(docs_filter, docs)), key=lambda doc: doc.get('order_position'))))
 
 
 @bp.route('/<string:key>')
--- api/blueprints/sponsors.py
+++ api/blueprints/sponsors.py
@@ -1,19 +1,32 @@
+import datetime
+from config import BUCKETS, FOLDERS
 from flask import Blueprint, jsonify, request, abort
+from firebase_admin import storage
 from services.realtime_collection import realtime_collections, Collections
 
 bp = Blueprint('sponsors', __name__, url_prefix='/sponsors')
 
+def to_obj(doc):
+    doc_copy = doc.copy()
+    doc_copy['fileurl'] = [
+        storage.bucket(BUCKETS['default']) \
+            .blob(f'{FOLDERS["sponsors"]}/{doc["filename"]}') \
+            .generate_signed_url(
+            expiration=datetime.timedelta(days=1),
+        )
+    ]
+
+    return doc_copy
 
 @bp.route('/')
 def index():
-    pass
     project = request.args.get('project', 'cbeci')
     docs = realtime_collections.collections[Collections.SPONSORS].get()
 
     def docs_filter(doc):
         return doc.get('is_active') == True and doc.get('project') == project
 
-    return jsonify(data=list(sorted(filter(docs_filter, docs), key=lambda doc: doc.get('order_position'))))
+    return jsonify(data=list(sorted(map(to_obj, filter(docs_filter, docs)), key=lambda doc: doc.get('order_position'))))
 
 
 @bp.route('/<string:key>')

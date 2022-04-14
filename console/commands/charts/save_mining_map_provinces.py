2cc298308824635ada8b2fc8d9e9496a3f8c8cb4
Ihor Radchenko <radchenko_io@groupbwt.com>
2022-04-14 15:22:27 +0300

    feat(ea-974): update mining map data

--- console/commands/charts/save_mining_map_provinces.py
+++ console/commands/charts/save_mining_map_provinces.py
@@ -6,17 +6,27 @@ from config import config
 
 
 @click.command(name='charts:save:mining-map-provinces')
+@click.option('-c', '--country', default='CN')
 @click.argument('filename')
-def handle(filename):
+def handle(filename, country):
     with open(filename) as file, psycopg2.connect(**config['custom_data']) as connection:
         reader = csv.reader(file)
         next(reader, None)
 
         cursor = connection.cursor()
+
+        cursor.execute('SELECT max(id) FROM mining_map_provinces')
+        key = cursor.fetchall()[0][0]
+
+        cursor.execute('SELECT id FROM countries WHERE code = %s', (country,))
+        country_id = cursor.fetchall()[0][0]
+
         for row in reader:
-            save(cursor, row)
+            key += 1
+            save(cursor, key, row, country_id)
 
 
-def save(cursor, row):
-    insert_sql = 'INSERT INTO mining_map_provinces (name, value, local_value, date) VALUES (%s, %s, %s, %s)'
-    cursor.execute(insert_sql, (row[1], 0, row[4], row[0]))
+def save(cursor, key, row, country_id):
+    insert_sql = 'INSERT INTO mining_map_provinces (id, name, value, local_value, date, country_id) ' \
+                 'VALUES (%s, %s, %s, %s, %s, %s)'
+    cursor.execute(insert_sql, (key, row[2], 0, row[5], row[0], country_id))

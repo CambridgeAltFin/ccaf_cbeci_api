4516b7c64718e2c28cefd975906dd41a9b968acd
Ihor Radchenko <radchenko1995@gmail.com>
2022-09-26 12:05:33 +0300

    feat(ea-1087): timestamp of start year

--- api/resources/gas_emission/total_yearly_bitcoin_greenhouse_gas_emission.py
+++ api/resources/gas_emission/total_yearly_bitcoin_greenhouse_gas_emission.py
@@ -1,9 +1,11 @@
+from datetime import datetime
+from dateutil import tz
 
 
 class TotalYearlyBitcoinGreenhouseGasEmissionChartPoint(dict):
     def __init__(self, point, date):
         super().__init__(
-            x=int(date.timestamp()),
+            x=int(datetime(date.year, 1, 1, tzinfo=tz.tzutc()).timestamp()),
             y=round(point['v'], 2),
             cumulative_y=round(point['cumulative_v'], 2),
         )

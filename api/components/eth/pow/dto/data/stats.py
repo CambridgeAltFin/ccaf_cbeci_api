5165a123e1850a9309b570bc3a6876418244897c
Ihor Radchenko <radchenko1995@gmail.com>
2023-01-27 12:36:27 +0200

    feat(cbnsi): change GW to kW, TWh to MWh

--- api/components/eth/pow/dto/data/stats.py
+++ api/components/eth/pow/dto/data/stats.py
@@ -1,10 +1,10 @@
 class StatsDto(dict):
     def __init__(self, stats):
         super().__init__({
-            'min_power': round(stats.get('min_power', 0), 2),
-            'guess_power': round(stats.get('guess_power', 0), 2),
-            'max_power': round(stats.get('max_power', 0), 2),
-            'min_consumption': round(stats.get('min_consumption', 0), 2),
-            'guess_consumption': round(stats.get('guess_consumption', 0), 2),
-            'max_consumption': round(stats.get('max_consumption', 0), 2),
+            'min_power': round(stats.get('min_power', 0) * 1_000_000, 2),
+            'guess_power': round(stats.get('guess_power', 0) * 1_000_000, 2),
+            'max_power': round(stats.get('max_power', 0) * 1_000_000, 2),
+            'min_consumption': round(stats.get('min_consumption', 0) * 1_000_000, 2),
+            'guess_consumption': round(stats.get('guess_consumption', 0) * 1_000_000, 2),
+            'max_consumption': round(stats.get('max_consumption', 0) * 1_000_000, 2),
         })
--- api/resources/data/stats_resource.py
+++ api/resources/data/stats_resource.py
@@ -3,10 +3,10 @@
 class StatsResource(dict):
     def __init__(self, item):
         super().__init__({
-            'guess_consumption': round(item['guess_consumption'], 2),
-            'guess_power': round(item['guess_power'], 2),
-            'max_consumption': round(item['max_consumption'], 2),
-            'max_power': round(item['max_power'], 2),
-            'min_consumption': round(item['min_consumption'], 2),
-            'min_power': round(item['min_power'], 2),
+            'guess_consumption': round(item['guess_consumption'] * 1_000_000, 2),
+            'guess_power': round(item['guess_power'] * 1_000_000, 2),
+            'max_consumption': round(item['max_consumption'] * 1_000_000, 2),
+            'max_power': round(item['max_power'] * 1_000_000, 2),
+            'min_consumption': round(item['min_consumption'] * 1_000_000, 2),
+            'min_power': round(item['min_power'] * 1_000_000, 2),
         })
